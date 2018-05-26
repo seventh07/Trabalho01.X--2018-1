dfaa670f4190f5b79a4ff85f110ea57c0ce603d2
Adrian Dellatorre <adrianld94@gmail.com>
2018-05-25 23:24:29 -0300

    Commit Inicial
    Commit Inicial

---
+++ app/src/androidTest/java/com/example/adrianld07/trabalho01x__2018_1/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.example.adrianld07.trabalho01x__2018_1;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.adrianld07.trabalho01x__2018_1", appContext.getPackageName());
+    }
+}
---
+++ app/src/main/java/com/example/adrianld07/trabalho01x__2018_1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.adrianld07.trabalho01x__2018_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
